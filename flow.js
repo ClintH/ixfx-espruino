1519a9131eb1de6a7ed1b6f9236e9f652379cc26
CH <clint@thestaticvoid.net>
2022-08-25 21:58:32 +0200

    Update

--- flow.js
+++ flow.js
@@ -1,54 +1,22 @@
-/* Copyright (c) 2022 CH. See the file LICENSE for copying permission. */
-/*
-flow
-*/
-
-var C = {
-  MY : 0x001,          // description
-  PRIVATE : 0x001,     // description
-  CONSTANTS : 0x00423  // description
-};
-
-function MOD123(pin1,pin2) {
-  this.pin1 = pin1;
-  this.pin2 = pin2;
-}
-
-/** 'public' constants here */
-MOD123.prototype.C = {
-  MY : 0x013,         // description
-  PUBLIC : 0x0541,    // description
-  CONSTANTS : 0x023   // description
-};
-
-/** Put most of my comments outside the functions... */
-MOD123.prototype.foo = function() {
-  // you can use C.PRIVATE
-  // or this.C.PUBLIC
-};
-
-/** Put most of my comments outside the functions... */
-MOD123.prototype.bar = function() {
-};
-
+/**
+ * ixfx flow
+ */
 function timeout(callback, timeoutMs) {
  let timer = 0;
  let startedAt = 0;
 
- const start = (altTimeoutMs) => {
+ const start = (altTimeoutMs, args) => {
   if (altTimeoutMs === undefined) altTimeoutMs = timeoutMs;
   if (timer !== 0) cancel();
-  console.log('timeout start');
+  startedAt = Date.now();
   timer = setTimeout(function() {
-    console.log('timeout invoking callback');
-    callback();
+    callback(Date.now() - startedAt, args);
     timer = 0;
   }, timeoutMs);
  }
  
  const cancel = () => {
   if (timer === 0) return;
-  console.log('timeout cancelling');
   startedAt = 0;
   clearTimeout(timer);
  }
@@ -69,7 +37,7 @@ function timeout(callback, timeoutMs) {
  */
 debounce = function (callback, timeoutMs) {
   const t = timeout(callback, timeoutMs);
-  const fn = () => t.start();
+  const fn = (args) => t.start(args);
   return fn;
 };
 
